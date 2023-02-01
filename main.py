77c8e8bb1c1a3b162478fcc6c8d4244bed5fb489
szlankapeter <szlankapeter@gmail.com>
2023-02-01 12:31:17 +0100

    összegzés

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 #megoldas
 def eredmeny(jPont, gPont):
-    if jPont > 21:
+    if pontOszz(jPont) > 21:
         return "játékos vesztett"
-    if gPont > 21:
+    if pontOszz(gPont) > 21:
         return "gép vesztett"
+
+def pontOszz(tomb):
+    osszeg = 0
+    i = 0
+    pont = 0
+    while i < len(tomb):
+        osszeg += tomb[i]
+        i += 1
+    return osszeg
+
+
 #teszteset
 
 
