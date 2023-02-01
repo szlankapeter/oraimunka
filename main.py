1da42281102ec29098a61d9f1c0e72228fffb585
szlankapeter <szlankapeter@gmail.com>
2023-02-01 12:14:49 +0100

    eredmény visszaadás

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #megoldas
-def eredmeny():
-    pass
+def eredmeny(jPont, gPont):
+    if jPont > 21:
+        return "játékos vesztett"
+    if gPont > 21:
+        return "gép vesztett"
 #teszteset
 
 
