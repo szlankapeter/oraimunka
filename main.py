53307f340c1b97672f86c05e72232fd9cd7ee3a1
szlankapeter <szlankapeter@gmail.com>
2023-02-01 13:20:27 +0100

    tesztek

--- main.py
+++ main.py
@@ -13,14 +13,55 @@ def pontOszz(tomb):
         osszeg += tomb[i]
         i += 1
     return osszeg
+
+
 #teszteset
 
 def jatekosVeszetettTeszt():
     jatekosPont = [9, 9, 9, 9]
-    gepPontok = [1, 1, 1, 1]
-    print(eredmeny(jatekosPont, gepPontok))
+    gepPontok = [7, 4, 5, 3]
+    kapott = eredmeny(jatekosPont, gepPontok)
+    vart = "játékos vesztett"
+    if kapott == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt sikertelen")
+
+
+def gepVeszetettTeszt():
+    jatekosPont = [7, 4, 5, 3]
+    gepPontok = [9, 9, 9, 9]
+    kapott = eredmeny(jatekosPont, gepPontok)
+    vart = "gép vesztett"
+    if kapott == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt sikertelen")
+
+def dontetlen():
+    jatekosPont = [7, 4, 5, 3]
+    gepPontok = [7, 4, 5, 3]
+    kapott = eredmeny(jatekosPont, gepPontok)
+    vart = "döntetlen"
+    if kapott == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt sikertelen")
+def mindenkiVesztett():
+    jatekosPont = [9, 9, 9, 9]
+    gepPontok = [9, 9, 9, 8]
+    kapott = eredmeny(jatekosPont, gepPontok)
+    vart = "mindkettő vesztett"
+    if kapott == vart:
+        print("A teszt sikeres")
+    else:
+        print("A teszt sikertelen")
+
 
 def tesztek():
     jatekosVeszetettTeszt()
+    gepVeszetettTeszt()
+    dontetlen()
+    mindenkiVesztett()
 
 tesztek()
